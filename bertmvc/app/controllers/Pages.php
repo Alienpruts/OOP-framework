ba04dc8bc0ed90f88b9d5930211502b47bfe49a3
Bert Van de Casteele (Alienpruts) <vandecasteele@evilones.be>
2018-09-10 17:10:59 +0200

    Chapter 3 : Added Pages controller for testing

---
+++ bertmvc/app/controllers/Pages.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bert
+ * Date: 10/09/18
+ * Time: 17:03
+ */
+
+class Pages
+{
+
+    /**
+     * Pages constructor.
+     */
+    public function __construct()
+    {
+        echo 'Pages loaded';
+    }
+}
