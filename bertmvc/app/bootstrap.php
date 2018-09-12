55ed6a8f39242336c9856180c8a6d23ee01fe993
Bert Van de Casteele (Alienpruts) <vandecasteele@evilones.be>
2018-09-12 11:08:07 +0200

    Chapter 4 : Added config file + switched to autoloading Core libraries

--- bertmvc/app/bootstrap.php
+++ bertmvc/app/bootstrap.php
@@ -6,9 +6,12 @@
  * Time: 14:04
  */
 
-require_once __DIR__ . '/libraries/Core.php';
-require_once __DIR__ . '/libraries/Controller.php';
-require_once __DIR__ . '/libraries/Database.php';
+require_once __DIR__ . '/config/config.php';
+
+// Autoloader Core Libraries
+spl_autoload_register(function ($className){
+    require_once 'libraries/' . $className . '.php';
+});
 
 
 
---
+++ bertmvc/app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bert
+ * Date: 12/09/18
+ * Time: 11:00
+ */
+
+// App Root
+define('APP_ROOT', dirname(dirname( __FILE__)));
+
+// URL Root
+define('URL_ROOT', 'http://localhost/bertmvc');
+
+// Site Name
+define('SITE_NAME', 'BertMVC');
