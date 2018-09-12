e486da74cde22267f26e9ed646e10c33d7ead3c5
Bert Van de Casteele (Alienpruts) <vandecasteele@evilones.be>
2018-09-12 15:32:16 +0200

    Chapter 4 : Modified Database class to execute prepared statements and return results + test in Model

--- bertmvc/app/libraries/Database.php
+++ bertmvc/app/libraries/Database.php
@@ -39,5 +39,81 @@ class Database
        }
     }
 
+    /**
+     * Set the statement using the supplied query.
+     * @param $sql
+     */
+    public function query($sql)
+    {
+        $this->stmt = $this->dbh->prepare($sql);
+    }
 
+    /**
+     * Bind named parameters to their values.
+     * @param $param
+     * @param $value
+     * @param null $type
+     */
+    public function bind($param, $value, $type = null)
+    {
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+
+            }
+
+            $this->stmt->bindValue($param, $value, $type);
+        }
+    }
+
+
+    /**
+     * Executes the prepared statement.
+     * @return mixed
+     */
+    public function execute()
+    {
+        return $this->stmt->execute();
+    }
+
+    /**
+     * Returns the resultset as an array of objects.
+     * @return mixed
+     */
+    public function resultSet()
+    {
+        $this->execute();
+
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    /**
+     * Returns a single result as an object.
+     * @return mixed
+     */
+    public function single()
+    {
+        $this->stmt->execute();
+
+        return $this->smtm->fetch(PDO::FETCH_OBJ);
+    }
+
+    /**
+     * Returns the number of rows in the resultset.
+     * @return mixed
+     */
+    public function rowCount()
+    {
+        return $this->stmt->rowCount();
+    }
 }
