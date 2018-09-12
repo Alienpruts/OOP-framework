7a742c3dfd498f639900813119b72a0d8d90a590
Bert Van de Casteele (Alienpruts) <vandecasteele@evilones.be>
2018-09-12 16:16:05 +0200

    Chapter 4 : Cleanup

--- bertmvc/app/config/config.php
+++ bertmvc/app/config/config.php
@@ -8,15 +8,15 @@
 
 // DB Parameters
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'wouldntyouliketoknow?');
-define('DB_NAME', 'testmvc');
+define('DB_USER', '_YOUR_USER_');
+define('DB_PASS', '_YOUR_PASS');
+define('DB_NAME', '_YOUR_DBNAME_');
 
 // App Root
 define('APP_ROOT', dirname(dirname( __FILE__)));
 
 // URL Root
-define('URL_ROOT', 'http://localhost/bertmvc');
+define('URL_ROOT', '_YOUR_URL');
 
 // Site Name
-define('SITE_NAME', 'BertMVC');
+define('SITE_NAME', '_YOUR_SITENAME_');
--- bertmvc/app/views/pages/index.php
+++ bertmvc/app/views/pages/index.php
@@ -3,11 +3,6 @@ require_once APP_ROOT . '/views/inc/header.php';
 ?>
 
 <h1><?php echo $data['title']; ?></h1>
-<ul>
-    <?php foreach ($data['posts'] as $post) : ?>
-    <li><?php echo $post->title; ?></li>
-    <?php endforeach; ?>
-</ul>
 
 <?php
 require_once APP_ROOT . '/views/inc/footer.php';
